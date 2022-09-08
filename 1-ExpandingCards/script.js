09cfe5928785fac3478e442fb59840477bfaff68
pankaj485 <pankajsamar2@gmail.com>
2022-09-08 17:03:11 +0530

    project1: Exapnding Cards

---
+++ 1-ExpandingCards/script.js
@@ -0,0 +1,12 @@
+const panels = document.querySelectorAll(".panel");
+
+const removeActiveClasses = () => {
+  panels.forEach((panel) => panel.classList.remove("active"));
+};
+
+panels.forEach((panel) => {
+  panel.addEventListener("click", () => {
+    removeActiveClasses();
+    panel.classList.add("active");
+  });
+});
